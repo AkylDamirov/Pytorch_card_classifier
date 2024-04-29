4a9a0f6fc2ca5e4a29c0d5c385a9e5c8646e89c5
Akyl <akyl@gmail.com>
2024-04-29 13:48:18 +0200

    final

--- card_classifier.py
+++ card_classifier.py
@@ -6,13 +6,17 @@ import torchvision
 import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
 import timm
-
+from PIL import Image
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import sys
 from tqdm.notebook import tqdm
+from glob import glob
 
+
+
+#Pytorch dataset
 class PlayingCardDataset(Dataset):
     def __init__(self, data_dir, transform=None):
         self.data = ImageFolder(data_dir, transform=transform)
@@ -28,11 +32,183 @@ class PlayingCardDataset(Dataset):
         return self.data.classes
 
 
-dataset = PlayingCardDataset(
-    data_dir='../../../Desktop/train'
-)
+# dataset = PlayingCardDataset(
+#     data_dir='train'
+# )
 
 # print(len(dataset))
+
+# image, label = dataset[6000]
+#
+# print(label)
+# print(image)
+
+# target_to_class = {v: k for k,v in ImageFolder(data_dir).class_to_idx.items()}
+# print(target_to_class)
+
+transform = transforms.Compose([
+    transforms.Resize((128,128)),
+    transforms.ToTensor(),
+])
+
+data_dir = 'train'
+dataset = PlayingCardDataset(data_dir, transform)
+
+# print(dataset[10])
+
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+for images, labels in dataloader:
+    break
+
+#Pytorch model
+class SimpleCardClassifer(nn.Module):
+    def __init__(self, num_classes=53):
+        super(SimpleCardClassifer, self).__init__()
+        self.base_model = timm.create_model('efficientnet_b0', pretrained=True)
+        self.features = nn.Sequential(*list(self.base_model.children())[:-1])
+
+        enet_out_size = 1280
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(enet_out_size, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        output = self.classifier(x)
+        return output
+
+model = SimpleCardClassifer(num_classes=53)
+# print(model)
+# print(str(model)[:500])
+# print(model(images))
+example = model(images)
+# print(a.shape)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# print(criterion(example, labels))
+
+#set Datasets
+
+train_folder = 'train'
+valid_folder = 'valid'
+test_folder = 'test'
+
+train_dataset = PlayingCardDataset(train_folder, transform=transform)
+valid_dataset = PlayingCardDataset(valid_folder, transform=transform)
+test_dataset = PlayingCardDataset(test_folder, transform=transform)
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+valid_loader = DataLoader(valid_dataset, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=True)
+
+#training loop
+num_epochs = 5
+train_losses, val_losses = [], []
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+model = SimpleCardClassifer(num_classes=53)
+model.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+for epoch in range(num_epochs):
+    model.train()
+    running_loss= 0.0
+    for images, labels in tqdm(train_loader, desc='Training loop'):
+        images, labels = images.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item() * labels.size(0)
+    train_loss = running_loss / len(train_loader.dataset)
+    train_losses.append(train_loss)
+
+    model.eval()
+    running_loss = 0.0
+    with torch.no_grad():
+        for images, labels in tqdm(valid_loader, desc='Validation loop'):
+            images, labels = images.to(device), labels.to(device)
+
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            running_loss += loss.item() * labels.size(0)
+
+    val_loss = running_loss / len(valid_loader.dataset)
+    val_losses.append(val_loss)
+    print(f'Epoch {epoch+1}/{num_epochs} - Train loss {train_loss}, Validation loss {val_loss}')
+
+plt.plot(train_losses, label='Training loss')
+plt.plot(val_losses, label='Validation loss')
+plt.legend()
+plt.title("Loss over epochs")
+plt.show()
+
+#evaluting results
+#load and preprocess the image
+def preprocess_image(image_path, transform):
+    image = Image.open(image_path).convert('RGB')
+    return image, transform(image).unsqueeze(0)
+
+#predict using model
+def predict(model, image_tensor, device):
+    model.eval()
+    with torch.no_grad():
+        image_tensor = image_tensor.to(device)
+        outputs = model(image_tensor)
+        probabilities = torch.nn.functional.softmax(outputs, dim=1)
+    return probabilities.cpu().numpy().flatten()
+
+#visualization
+def visualize_predictions(original_image, probabilities, class_names):
+    fig, axarr = plt.subplots(1,2, figsize=(14,7))
+
+    #display image
+    axarr[0].imshow(original_image)
+    axarr[0].axis('off')
+
+    #display predictions
+    axarr[1].barh(class_names, probabilities)
+    axarr[1].set_xlabel('Probability')
+    axarr[1].set_title('Class Predictions')
+    axarr[1].set_xlim(0, 1)
+
+    plt.tight_layout()
+    plt.show()
+
+#example usage
+test_image = 'test/five of diamonds/2.jpg'
+transform = transforms.Compose([
+    transforms.Resize((128,128)),
+    transforms.ToTensor()
+])
+original_image, image_tensor = preprocess_image(test_image, transform)
+probabilities = predict(model, image_tensor, device)
+
+# Assuming dataset.classes gives the class names
+class_names = dataset.classes
+visualize_predictions(original_image, probabilities, class_names)
+
+
+
+# test_images = glob('test/*/*')
+# test_examples = np.random.choice(test_images, 5)
+#
+# for example in test_examples:
+#     original_image, image_tensor = preprocess_image(example, transform)
+#     probabilities = predict(model, image_tensor,device)
+#
+#     #assuming dataset classes give names
+#     class_names = dataset.classes
+#     visualize_predictions(original_image, probabilities, class_names)
 
 
 
